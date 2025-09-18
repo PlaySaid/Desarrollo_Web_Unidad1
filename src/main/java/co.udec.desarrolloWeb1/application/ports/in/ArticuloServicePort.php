f5dd327b1adac6d66f6810b826f43c3d4eca8c6f
saidm <smartelod@unicartagena.edu.co>
2025-09-18 11:40:27 -0500

    Se agregaron los puertos IN y OUT con todos los metodos requeridos para el programa

---
+++ src/main/java/co.udec.desarrolloWeb1/application/ports/in/ArticuloServicePort.php
@@ -0,0 +1,10 @@
+<?php
+
+interface ArticuloServicePort
+{
+    public function crearArticulo(array $datos);
+    public function obtenerArticuloPorId(int $id);
+    public function actualizarArticulo(int $id, array $datos);
+    public function eliminarArticulo(int $id);
+    public function listarArticulos();
+}
---
+++ src/main/java/co.udec.desarrolloWeb1/application/ports/out/ArticuloRepositoryPort.php
@@ -0,0 +1,9 @@
+<?php
+
+interface ArticuloRepositoryPort {
+    public function guardar(Articulo $articulo);
+    public function buscarPorId(int $id): ?Articulo;
+    public function actualizar(int $id, Articulo $articulo);
+    public function eliminar(int $id);
+    public function listar(): array;
+}
